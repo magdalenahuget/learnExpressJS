70cd2b980c7d161fb086445e856150f5839408d3
magdalenahuget <magdalena.huget@gmail.com>
2023-02-07 13:08:52 +0100

    Add solution - until 21 minute

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('User List')
+})
+
+router.get('/new', (req, res) => {
+    res.send("User New Form")
+})
+
+
+router.post('/new', (req, res) => {
+    res.send("Create User")
+})
+
+router
+.route("/:id")
+.get((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+}).put((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+}).delete((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+})
+
+module.exports = router;
